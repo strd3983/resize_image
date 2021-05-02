68bfaaf257866f3c3a062fdec8bc8b25105c7c4e
strd3983 <strd3983@gmail.com>
2021-05-02 17:41:23 +0900

    v1.1 - unset ReadOnly

--- resize_image.py
+++ resize_image.py
@@ -1,5 +1,23 @@
-import os, imghdr
+import os
+import ctypes
+import imghdr
 from PIL import Image
+
+FILE_ATTRIBUTE_HIDDEN: int = 2
+FILE_ATTRIBUTE_READONLY: int = 1
+TARGETBITS: int = FILE_ATTRIBUTE_READONLY + FILE_ATTRIBUTE_HIDDEN
+REV_TARGETBITS = ~TARGETBITS
+
+
+# WindowsファイルのReadonlyと隠し属性を
+# 未設定状態に設定する。
+def unsetReadonlyAttrib(file_path):
+    # Windowsファイル属性を取得
+    val: int = ctypes.windll.kernel32.GetFileAttributesW(file_path)
+    if 0 != (val & 3):
+        val = val & REV_TARGETBITS
+        # Windowsファイル属性を再設定
+        ctypes.windll.kernel32.SetFileAttributesW(file_path, val)
 
 
 def resize_img_file(file_path):
@@ -8,6 +26,8 @@ def resize_img_file(file_path):
     if img_type is None:
         return
 
+    # 指定パスのファイル属性を解除
+    unsetReadonlyAttrib(file_path)
     img = Image.open(file_path).convert('RGB')
     fp_remove = file_path
 
@@ -16,15 +36,15 @@ def resize_img_file(file_path):
     if MAX_SIZE <= 1280:
         if os.path.splitext(file_path)[1] == '.png':
             file_path = os.path.join(
-                os.path.dirname(file_path),
-                os.path.splitext(os.path.basename(file_path))[0] + '.jpg')
+                os.path.dirname(file_path), os.path.splitext(
+                    os.path.basename(file_path))[0] + '.jpg')
             img.save(file_path, "JPEG")
             del img
             os.remove(fp_remove)
         return
     elif img.width > img.height:
-        img_resize = img.resize((1280, round(img.height * 1280 / img.width)),
-                                Image.LANCZOS)
+        img_resize = img.resize(
+            (1280, round(img.height * 1280 / img.width)), Image.LANCZOS)
     else:
         img_resize = img.resize((round(img.width * 1280 / img.height), 1280),
                                 Image.LANCZOS)
