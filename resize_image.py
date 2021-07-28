43494ac90c96766698e24f827030c628bcb3bf32
strd3983 <strd3983@gmail.com>
2021-07-28 16:42:25 +0900

    v2.0 - 長編サイズの指定、処理アルゴリズムを選択可能に

--- resize_image.py
+++ resize_image.py
@@ -1,90 +1,142 @@
 import os
 import ctypes
 import imghdr
+import configparser
 from PIL import Image
-Image.LOAD_TRUNCATED_IMAGES = True
 
+Image.LOAD_TRUNCATED_IMAGES = True
 FILE_ATTRIBUTE_HIDDEN: int = 2
 FILE_ATTRIBUTE_READONLY: int = 1
 TARGETBITS: int = FILE_ATTRIBUTE_READONLY + FILE_ATTRIBUTE_HIDDEN
 REV_TARGETBITS = ~TARGETBITS
-rSIZE: int = 1280
 
 
-# ファイルが画像かどうか判定する
-def file_type(file_path):
-    f = open(file_path, 'rb')
+# --------------------------------------------------
+# メイン関数
+# --------------------------------------------------
+def main():
+    rSIZE = 0  # リサイズサイズ
+    rTYPE = 6  # フィルタタイプ
+    rSIZE, rTYPE = config()
+    if rSIZE <= 0 or rTYPE < 0 or 5 < rTYPE:
+        print('=> setting.iniの値が不適切です\n')
+        return
+    dir = os.getcwd()
+    try:
+        recursive_resize_img_file(dir, rSIZE, rTYPE)
+    except OSError:
+        print('画像読み込みエラー')
+
+
+# --------------------------------------------------
+# read_file()関数によるiniファイルの読み込み
+# --------------------------------------------------
+def config():
+    global rSIZE
+    global rTYPE
+    config_ini = configparser.ConfigParser()
+    config_ini_path = 'setting.ini'
+    # iniファイルが存在するかチェック
+    if os.path.exists(config_ini_path):
+        # iniファイルが存在する場合、ファイルを読み込む
+        with open(config_ini_path, encoding='utf-8') as fp:
+            config_ini.read_file(fp)
+            # iniの値取得
+            read_default = config_ini['DEFAULT']
+            rSIZE = read_default.get('長辺サイズ')
+            rTYPE = read_default.get('アルゴリズム')
+            # 設定出力
+            print('#---------------------------------#')
+            print('長辺サイズ:', rSIZE)
+            print('リサイズアルゴリズム:', rTYPE)
+            print('#---------------------------------#')
+            return int(rSIZE), int(rTYPE)
+    else:
+        print('setting.iniが見つかりません\n')
+        return 0, 6
+
+
+# --------------------------------------------------
+# ファイル探索
+# --------------------------------------------------
+def recursive_resize_img_file(fp, rSIZE, rTYPE):
+    if os.path.isdir(fp):  # ディレクトリだった場合
+        print(fp)
+        files = os.listdir(fp)
+        for file in files:
+            recursive_resize_img_file(os.path.join(fp, file), rSIZE, rTYPE)
+    else:  # ファイルだった場合
+        resize_img_file(fp, rSIZE, rTYPE)
+
+
+# --------------------------------------------------
+# ファイルの画像判定
+# --------------------------------------------------
+def file_type(fp):
+    f = open(fp, 'rb')
     f_head = f.read()[:2]
     f.close()
     if f_head == b'\xff\xd8':
         return 'jpeg'
     else:
-        return imghdr.what(file_path)
+        return imghdr.what(fp)
 
 
-# WindowsファイルのReadonlyと隠し属性を未設定状態に設定する。
-def unsetReadonlyAttrib(file_path):
+# --------------------------------------------------
+# ファイルの属性解除
+# --------------------------------------------------
+def unsetReadonlyAttrib(fp):
     # Windowsファイル属性を取得
-    val: int = ctypes.windll.kernel32.GetFileAttributesW(file_path)
+    val: int = ctypes.windll.kernel32.GetFileAttributesW(fp)
     if 0 != (val & 3):
         val = val & REV_TARGETBITS
         # Windowsファイル属性を再設定
-        ctypes.windll.kernel32.SetFileAttributesW(file_path, val)
+        ctypes.windll.kernel32.SetFileAttributesW(fp, val)
 
 
-def resize_img_file(file_path):
+# --------------------------------------------------
+# リサイズ処理
+# --------------------------------------------------
+def resize_img_file(fp, rSIZE, rTYPE):
     # ファイルが画像ファイルかどうかを確認し、画像ファイルではない場合リサイズ処理は行わない
-    img_type = file_type(file_path)
+    img_type = file_type(fp)
     if img_type is None:
         return
 
     # 指定パスのファイル属性を解除
-    unsetReadonlyAttrib(file_path)
-    img = Image.open(file_path).convert('RGB')
-    fp_remove = file_path
+    unsetReadonlyAttrib(fp)
+    img = Image.open(fp).convert('RGB')
 
-    # 長編サイズが1280px(rSIZE)以下のときはスルー
+    # 長編サイズがrSIZE px以下のときはスルー
     MAX_SIZE = max(img.width, img.height)
+    lSIZE = round(img.height * rSIZE / img.width)
     if MAX_SIZE <= rSIZE:
-        if img_type == 'png':
-            file_path = os.path.join(
-                os.path.dirname(file_path),
-                os.path.splitext(os.path.basename(file_path))[0] + '.jpg')
-            img.save(file_path, "JPEG")
-            del img
-            os.remove(fp_remove)
+        convert_type(fp, img)
         return
     elif img.width > img.height:
-        img_resize = img.resize((rSIZE, round(img.height * rSIZE / img.width)),
-                                Image.LANCZOS)
+        img_resize = img.resize((rSIZE, lSIZE), rTYPE)
     else:
-        img_resize = img.resize((round(img.width * rSIZE / img.height), rSIZE),
-                                Image.LANCZOS)
+        img_resize = img.resize((lSIZE, rSIZE), rTYPE)
+    convert_type(fp, img_resize)
 
-    # ファイル名が.pngである場合.jpgに変換
-    if os.path.splitext(file_path)[1] == '.png':
-        file_path = os.path.join(
-            os.path.dirname(file_path),
-            os.path.splitext(os.path.basename(file_path))[0] + '.jpg')
-        img_resize.save(file_path, "JPEG")
+
+# --------------------------------------------------
+# ファイルが.pngである場合.jpgに変換
+# --------------------------------------------------
+def convert_type(fp, img):
+    fp_remove = fp
+    if os.path.splitext(fp)[1] == '.png':
+        fp = os.path.join(os.path.dirname(fp),
+                          os.path.splitext(os.path.basename(fp))[0] + '.jpg')
+        img.save(fp, "JPEG")
         del img
         os.remove(fp_remove)
     else:
-        img_resize.save(file_path, "JPEG")
+        img.save(fp, "JPEG")
         del img
 
 
-def recursive_resize_img_file(file_path):
-    if os.path.isdir(file_path):
-        print(file_path)
-        files = os.listdir(file_path)
-        for file in files:
-            recursive_resize_img_file(os.path.join(file_path, file))
-    else:
-        resize_img_file(file_path)
-
-
-dir = os.getcwd()
-recursive_resize_img_file(dir)
-print('終了しました')
-os.system('PAUSE')
+if __name__ == "__main__":
+    main()
+    print('終了しました')
+    os.system('PAUSE')
